84ef8c778697861c635448095b5f2337bd7f712a
sahanah-ganesh <sahanah.ganesh@gmail.com>
2019-04-20 20:04:45 +0000

    refactored code based on js and css  beautify. did not work for html so tried to refactor manually

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -5,18 +5,18 @@ const simulateDelay = require("./util/simulate-delay");
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
-  return {
+    return {
 
-    // Saves a tweet to `db`
-    saveTweet: function(newTweet, callback) {
-      db.collection("tweets").insertOne(newTweet);
-      callback(null, true);
-    },
+        // Saves a tweet to `db`
+        saveTweet: function(newTweet, callback) {
+            db.collection("tweets").insertOne(newTweet);
+            callback(null, true);
+        },
 
-    // Get all tweets in `db`, sorted by newest first
-    getTweets: function(callback) {
-      db.collection("tweets").find().toArray(callback);
-    }
+        // Get all tweets in `db`, sorted by newest first
+        getTweets: function(callback) {
+            db.collection("tweets").find().toArray(callback);
+        }
 
-  };
+    };
 }
