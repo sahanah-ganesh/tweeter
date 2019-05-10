a3384732f851061cf1f1b2bf4981fc0544f476b8
sahanah-ganesh <sahanah.ganesh@gmail.com>
2019-05-10 19:35:43 +0000

    fixing mongodb issues env

--- server/index.js
+++ server/index.js
@@ -3,13 +3,13 @@
 // Basic express setup:
 require('dotenv').config();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
 const MongoClient = require("mongodb").MongoClient;
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/tweeter";
 
 app.use(bodyParser.urlencoded({
     extended: true
